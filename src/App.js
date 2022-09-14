dd076d7069b9c284f41ed21bf20c2fbddfa1ee42
hima-94 <himasri8143@gmail.com>
2022-09-14 10:35:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div><br/><br/>
+      <div class="d-flex justify-content-xl-center jumbotron jumbotron-fluid rounded">
+  <div class="container">
+      <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">sharing <b>Akhil Chakravarthy's</b> Tako Card</label> <br/><br/><br/>
+    <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" size="50" maxlength="4" placeholder="Full Name"/><br/> 
+    <div class="custom-control custom-radio">
+  <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input"/>
+  <label class="custom-control-label" for="customRadio1">WhatsApp</label>
+</div>
+<div class="custom-control custom-radio">
+  <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input"/>
+  <label class="custom-control-label" for="customRadio2">SMS</label>
+</div>
+<div class="custom-control custom-radio">
+  <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input"/>
+  <label class="custom-control-label" for="customRadio2">Email</label>
+</div>
+  </div><br/>
+  <div class="form-group">
+   
+    <input type="password" class="form-control" id="exampleInputPassword1"  size="50" maxlength="4"  placeholder="EnterPhoneNumber/EnterEmail "/>
+  </div><br/>
+ 
+  <button type="submit" class="btn btn-primary but">Submit</button>
+</form>
+</div>
+</div>
+     
     </div>
   );
 }
